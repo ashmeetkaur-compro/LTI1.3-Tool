2fa5ac37c2ffe00e452c464bf0738e35e6bcb67b
Ashmeet Kaur <ashmeet.kaur@comprotechnologies.com>
2019-10-14 19:08:13 +0530

    Structure changed

--- controllers/controller.js
+++ controllers/controller.js
@@ -1,37 +1,91 @@
-const platformData = require('../data/platform-data');
 const jwt = require('jsonwebtoken');
+const nonce = require('nonce-generator');
+const platformData = require('../data/platform-data');
+const toolData = require('../data/toolData');
+const keys = require('../data/keys');
 
 exports.defaultLaunch = (req,res) => {
   res.send("Default Launch successful");
 };
 
 exports.cretaeOidcResponse = (req,res) => {
-  let init_login_Request = req.body;
+  let privateKEY = keys.private_key;
 
-  if(init_login_Request.iss != platformData.iss) {
-    res.send(['Issuer invalid: not registered']);
+  if(req.body.client_id != platformData.client_id) {
+    // res.send(['Issuer invalid: not registered']);
+    res.render('error.ejs',{platformNotRegistered : true});
   }
 
-  res.send ({
-    scope: 'openid',
-    response_type: 'id_token',
-    client_id: 'SDF7ASDLSFDS9',
-    redirect_uri: 'https://piedpiper.localtunnel.me/project/submit',
-    login_hint: init_login_Request.login_hint,
-    state: create_unique_string(30, true),
-    response_mode: 'form_post',
-    prompt: 'none'
+  var initLoginParams = {
+    "client_id":  platformData.client_id,
+    "login_hint": req.body.login_hint,
+    "lti_message_hint": req.body.lti_message_hint,
+    "nonce":  nonce(20),
+    "prompt": "none",
+    "redirect_uri": toolData.launch_url,
+    "response_mode": "form_post",
+    "response_type":  "id_token",
+    "scope":  "openid"
+};
+
+var payload = {
+    "tool_id": 536,
+    "state_nonce": initLoginParams.nonce,
+    "params": {
+      "iss": req.body.iss,
+      "client_id": req.body.client_id,
+      "lti_deployment_id": req.body.lti_deployment_id,
+      "target_link_uri": toolData.launch_url,
+      "login_hint": req.body.login_hint,
+      "lti_message_hint": req.body.lti_message_hint,
+      "commit": req.body.commit,
+      "controller": "lti/login_initiations",
+      "action": "create",
+      "tool_id": "536"
+    },
+    "iss": toolData.launch_url,
+    "sub": platformData.client_id,
+    "aud": "",
+    "iat": Date.now(),
+    "exp": Date.now() + 300,
+    "jti": nonce(20)
+  };
+
+  const token = jwt.sign(payload, privateKEY, { algorithm: 'RS256', keyid:nonce(20)});
+
+  initLoginParams.state = token;
+
+  return res.render('initLogin.ejs' ,{
+    loginResParams : initLoginParams, 
+    action: platformData.oidc_auth_url
+  })
+};
+
+function create_unique_string(length, signed) {
+  let unique_string = '';
+  const possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  for(let i = 0; i < length; i++) {
+    unique_string += possible.charAt(Math.floor(Math.random() * possible.length));
+  }
+  //TODO: if signed === true, sign the string with our private key
+  return unique_string;
+}
+
+
+exports.validateLaunch = (req,res) => {
+  jwt.verify(req.body.id_token, platformData.publicKey, (err, decoded) => {
+    if(err) {
+    res.render('error.ejs' , {invalidSignature : true});
+    }
+    
+    return res.redirect(url.format({
+      pathname: toolData.display_url,
+      query: decoded
+    }));
   });
 };
 
-exports.launchTool = (req,res) => {
-  const jwt_string = req.body.id_token;
-  let basic_decoded = jwt.decode(jwt_string, {complete: true});
-};
-
-exports.generateToken = (req,res) => { 
-};
-
 exports.displayLaucnhPage = (req,res) => {
+  return res.render('launch.ejs', {});
 };
 
--- routes.js
+++ routes.js
@@ -3,17 +3,14 @@ const controller = require('./controllers/controller');
 
 router.get('/', controller.defaultLaunch);
 
-//TOOL:  OpenID Connect validation flow
+//TOOL:  OpenID Connect validation flow - Not yet implemented
 router.get('/oidc', controller.cretaeOidcResponse);
 
 //TOOL:  OpenID Connect validation flow
 router.post('/oidc', controller.cretaeOidcResponse);
 
-//TOOL: Generate Token
-router.post("/oauth2/token", controller.generateToken);
-
 //TOOL: Validate launch request and launch
-router.post('/project/submit', controller.launchTool);
+router.post('/project/submit', controller.validateLaunch);
 
 //TOOL: Display launch page
 router.get("/project/submit", controller.displayLaucnhPage);
