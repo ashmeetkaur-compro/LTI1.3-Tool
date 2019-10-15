d1d32d429f7d53c131ece4e44a827039268de441
Ashmeet Kaur <ashmeet.kaur@comprotechnologies.com>
2019-10-15 14:32:40 +0530

    launch.ejs updated

--- controllers/controller.js
+++ controllers/controller.js
@@ -82,6 +82,7 @@ exports.validateLaunch = (req,res) => {
 };
 
 exports.displayLaucnhPage = (req,res) => {
+  console.log(req.query)
   return res.render('launch.ejs', {
     params : req.query
   });
